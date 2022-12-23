025ee34b24916a1f83585362ee5b1254db07b412
DmitriiLipuntsov <55542391+DmitriiLipuntsov@users.noreply.github.com>
2022-12-23 08:07:14 +0800

    initial commit

---
+++ SimpleStore/SimpleStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStoreApp.swift
+//  SimpleStore
+//
+//  Created by Dmitry Lipuntsov on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
